26a7822f34f65e0dc7467f7da97d1104e006c882
Pawanreddy <ponereddy@gmail.com>
2025-03-14 08:47:25 +0530

    login api

--- src/Controller/UserController.js
+++ src/Controller/UserController.js
@@ -1,7 +1,13 @@
 const users = require("../Models/Users");
 exports.addUser = async (req,resp,next)=>{
     try {
+        resp.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+            resp.header(
+                'Access-Control-Allow-Headers',
+                'Origin, X-Requested-With, Content-Type, Accept'
+            );
         const user=  await users.addUser(req.body);
+
         resp.send("User Is added success");
     } catch (error) {
         resp.send("User Is not added ");
@@ -10,12 +16,24 @@ exports.addUser = async (req,resp,next)=>{
 exports.getLogDetails = async (req,resp,next)=>{
 
      try {
+        resp.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+        resp.header(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept'
+        );
         const {emailId,password} = req.body;
         const arrResult =  await  users.getUsersLoginDeatils(emailId,password);
-       resp.send(arrResult);
+        if(arrResult.length>0){
+            resp.cookie('_id', arrResult[0]._id);
+            resp.status(200).send(arrResult);
+
+        }else{
+            resp.status(401).send("Please check the user details");
+        }
+       
               
     } catch (error) {
-        resp.send("Please check the user details ");  
+        resp.status(401).send("Please check the user details ");  
     }
            
 }
